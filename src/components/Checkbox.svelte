dd866774e063d2d202085ec76f36f490f5c1ce2b
Alexander Hofbauer <a.hofbauer@fify.at>
2026-04-28 09:17:33 +0200

    Name-space loading data attribute

--- src/components/Checkbox.svelte
+++ src/components/Checkbox.svelte
@@ -15,7 +15,7 @@
 	}
 </script>
 
-<div class="altcha-checkbox" data-loading={loading}>
+<div class="altcha-checkbox" data-altcha-loading={loading}>
 	<input bind:this={inputEl} type="checkbox" {...rest} />
 	<svg aria-hidden="true" width="12" height="9" viewBox="0 0 12 9" onclick={onClick}>
 		<polyline points="1 5 4 8 11 1"></polyline>
