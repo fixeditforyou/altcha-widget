5db7e09c12f80972f10f6e9c3c59187d4c8cac09
Alexander Hofbauer <a.hofbauer@fify.at>
2026-07-13 09:36:15 +0200

    Rebuild v3.2.1-1

--- dist/external/altcha.js
+++ dist/external/altcha.js
@@ -5285,7 +5285,7 @@ function Checkbox($$anchor, $$props) {
   var svg = sibling(input, 2);
   next(2);
   reset(div);
-  template_effect(() => set_attribute(div, "data-loading", loading()));
+  template_effect(() => set_attribute(div, "data-altcha-loading", loading()));
   delegated("click", svg, onClick);
   append($$anchor, div);
   return pop($$exports);
@@ -5310,7 +5310,7 @@ function CheckboxNative($$anchor, $$props) {
   attribute_effect(input, () => ({ type: "checkbox", ...rest }), void 0, void 0, void 0, void 0, true);
   next(2);
   reset(div);
-  template_effect(() => set_attribute(div, "data-loading", loading()));
+  template_effect(() => set_attribute(div, "data-altcha-loading", loading()));
   append($$anchor, div);
   return pop($$exports);
 }
@@ -5403,7 +5403,7 @@ function Switch($$anchor, $$props) {
   bind_this(input, ($$value) => inputEl = $$value, () => inputEl);
   var div_1 = sibling(input, 2);
   reset(div);
-  template_effect(() => set_attribute(div, "data-loading", loading()));
+  template_effect(() => set_attribute(div, "data-altcha-loading", loading()));
   delegated("click", div_1, onClick);
   append($$anchor, div);
   return pop($$exports);
@@ -6333,7 +6333,7 @@ function Widget($$anchor, $$props) {
     }
   });
   onMount(() => {
-    log("mounted", "3.2.1");
+    log("mounted", "3.2.1-1");
     if (instance) {
       globalThis.$altcha.instances.add(instance);
     }
